0f05bfd6e3fd59e2c64d17194a6f058501d0a75f
pedram-macbook <pedram@email.com>
2024-06-26 20:25:14 +0330

    add order-product orm and relation

--- ProductOrder.py
+++ ProductOrder.py
@@ -9,8 +9,8 @@
 
 """
 from typing import Optional
-from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
-from sqlalchemy import Integer,VARCHAR,BIGINT,ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, VARCHAR, BIGINT, ForeignKey
 from mixins import TimestampMixin, TableNameMixin
 
 
@@ -18,9 +18,9 @@ class Base(DeclarativeBase):
     pass
 
 
-class Product(Base,TableNameMixin,TimestampMixin):
+class Product(Base, TableNameMixin, TimestampMixin):
     product_id = Mapped[int] = mapped_column(
-        Integer,primary_key=True
+        Integer, primary_key=True
     )
     title = Mapped[str] = mapped_column(
         VARCHAR(255)
@@ -28,6 +28,7 @@ class Product(Base,TableNameMixin,TimestampMixin):
     description = Mapped[Optional[str]] = mapped_column(
         VARCHAR(255)
     )
+
 
 """
     create sqlalchemy orm table without this value:
@@ -42,11 +43,35 @@ class Product(Base,TableNameMixin,TimestampMixin):
 """
 
 
-class Order(Base,TimestampMixin,TableNameMixin):
+class Order(Base, TimestampMixin, TableNameMixin):
     order_id = Mapped[int] = mapped_column(
-        Integer , primary_key=True
+        Integer, primary_key=True
     )
     user_id = Mapped[int] = mapped_column(
-        BIGINT , ForeignKey("users.telegram_id", ondelete="SET NULL")
+        BIGINT, ForeignKey("users.telegram_id", ondelete="SET NULL")
     )
 
+
+"""
+    create sqlalchemy orm table without this value:
+    CREATE TABLE order_products(
+        order_id = INTEGER NOT NULL,
+        product_id = INTEGER NOT NULL,
+        quantity = INTEGER NOT NULL,
+        FOREIGN KEY (order_id)
+            REFERENCES orders(order_id)
+            ON DELETE CASCADE,
+        FOREIGN KEY (product_id)
+            REFERENCES products(product_id)
+            ON DELETE CASCADE,);
+"""
+
+
+class OrderProduct(Base,TableNameMixin):
+    order_id = Mapped[int]=mapped_column(
+        Integer,ForeignKey("orders.order_id",ondelete="CASCADE"),primary_key=True
+    )
+    product_id = Mapped[int] = mapped_column(
+        Integer, ForeignKey("products.product_id",ondelete="RESTRICT"),primary_key=True
+    )
+    quantity = Mapped[int]
